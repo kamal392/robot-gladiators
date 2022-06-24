0201335d19608a8f530e07804404a5b849712eb0
kamal kishore <kamal.kishore981@gmail.com>
2022-06-23 23:48:11 -0400

    first js commit

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,43 @@
-window.alert("This is an alert! JavaScript is running!");
+ window.alert("This is an alert! JavaScript is running!");
+
+// in the first step we have defined our function but didnt call it
+
+function fight(){
+    window.alert("The fight has began!");
+ }
+var playerName = window.prompt("what is your robot name ?");
+
+// window.alert(playerName);
+
+console.log(playerName);
+
+console.log ("This logs a string , good for leaving yourself a message");
+//this will do match and log 20
+
+console.log(10+10);
+
+//what is this
+console.log("our robot name is" + playerName);
+
+var playerName = "Tony the robot"
+
+//Tony the Robot is ready for battle!
+console.log("Tony the Robot " + "is ready for a battle!");
+
+//Tony the robot ready for battle!
+console.log(playerName + " is ready for battle!");
+
+// your robot, tony the robot, has won!
+
+console.log("your robot, " + playerName + ", has won!");
+
+var name ="your name";
+
+
+
+
+
+// here i will be calling function by adding function name whihc is fight in this case with parentheses
+
+// fight();
+
