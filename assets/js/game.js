2d327951fc7a474237ed0ec8a3ba5668ef7fbce2
kamal kishore <kamal.kishore981@gmail.com>
2023-03-04 17:18:51 +0530

    creted function to communicate with players

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,39 @@
-window.alert("This is an alert! javaScript!");
+// this creates a function named "fight"
+
+function fight() {
+  window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is your robot's name?");
+
+window.alert(playerName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fight();
